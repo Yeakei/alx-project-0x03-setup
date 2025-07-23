85c37ebe3fe2ea4346a0e4e03cdec81b37195743
Yahya CHAKIRI <yahyachakiridev@gmail.com>
2025-07-23 21:38:03 +0100

    feat: add interface

---
+++ alx-project-0x03/interface/index.tsx
@@ -0,0 +1,16 @@
+export interface ButtonProps {
+    buttonLabel: string;
+    buttonSize?: string;
+    buttonBackgroundColor?: "red" | "blue" | "orange" | "green";
+    action?: () => void;
+}
+
+export interface PageRouteProps {
+    pageRoute: string;
+}
+
+import { ReactNode } from "react";
+
+export interface LayoutProps {
+    children: ReactNode;
+}
